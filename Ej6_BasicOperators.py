ae08cee6ca46283983dff6215b5f7cd8f1a5f8a8
Aarón Naveira <92159307+aaronNT12@users.noreply.github.com>
2024-01-12 12:29:47 +0100

    Add files via upload

---
+++ Ej6_BasicOperators.py
@@ -0,0 +1,42 @@
+#ARITHMETIC OPERATORS
+
+number = 1 + 2 * 3 / 4.0
+print(number)
+#------------------------#
+remainder = 11 % 3
+print(remainder)
+
+#------------------------#
+
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+#-------------------------#
+# USING OPERATORS WITH STRINGS#
+
+helloworld = "hello" +" " + "world"
+print(helloworld)
+
+#--------------------------#
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+#--------------------------#
+# USING OPERATORS WITH LISTS#
+
+even_numbers = [2,4,6,8]
+odd_numbers =[1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+#-------------------------#
+
+print([1,2,3] * 3)
+
+
+
+
+
